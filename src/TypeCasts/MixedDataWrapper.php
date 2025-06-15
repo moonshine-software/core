16f2c08a9e968baaa1e2aca4d85e52ffda2b0d6d
Danil Shutsky <leetoplayaz@yandex.ru>
2025-06-15 12:59:35 +0300

    feat(types): DataWrapper
    BREAKING-CHANGE: findItem, getItems, save, delete methods in CrudResource

--- src/TypeCasts/MixedDataWrapper.php
+++ src/TypeCasts/MixedDataWrapper.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace MoonShine\Core\TypeCasts;
 
+use ArrayAccess;
 use MoonShine\Contracts\Core\TypeCasts\DataWrapperContract;
 
-final readonly class MixedDataWrapper implements DataWrapperContract
+final readonly class MixedDataWrapper implements DataWrapperContract, ArrayAccess
 {
     public function __construct(private mixed $data, private string|int|null $key = null)
     {
@@ -30,4 +31,28 @@ final readonly class MixedDataWrapper implements DataWrapperContract
 
         return (array) $this->data;
     }
+
+    public function offsetExists(mixed $offset): bool
+    {
+        if(\is_array($this->data)) {
+            return isset($this->data[$offset]);
+        }
+
+        return \property_exists($this->data, $offset);
+    }
+
+    public function offsetGet(mixed $offset): mixed
+    {
+        if(\is_array($this->data)) {
+            return $this->data[$offset];
+        }
+
+        return $this->data->{$offset};
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void
+    {}
+
+    public function offsetUnset(mixed $offset): void
+    {}
 }
